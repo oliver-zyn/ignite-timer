b7e23cffd5f828e074dadb03eb3e9b0b9dc0e238
oliver-zyn <olivermayer15@gmail.com>
2023-06-09 20:42:48 -0300

    movendo global.ts para fora da pasta themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
-import { GlobalStyle } from './styles/themes/global'
+import { GlobalStyle } from './styles/global'
 import { Router } from './Router'
 import { BrowserRouter } from 'react-router-dom'
 import { CyclesContextProvider } from './contexts/CyclesContext'
